a9f01c181a1dd8b0710ca08ed82bb2ca1e7efb80
teffcode <tefy.1@live.com>
2018-10-17 13:41:11 -0500

    ⚙ Environment Variables Configuration

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.createConnection('mongodb://ds233218.mlab.com:33218/game-of-drones', {
+const db = mongoose.createConnection(process.env.MONGODB_URI, {
   auth: {
-    user: 'gameUser',
-    password: 'tefa123'
+    user: process.env.MONGODB_USER,
+    password: process.env.MONGODB_PWD
   }
 }); 
 
 db.on('connected', () => console.log('DB connected :)'));
 
 module.exports = db;
+
+// export MONGODB_URI='mongodb://ds233218.mlab.com:33218/game-of-drones' MONGODB_USER='gameUser' MONGODB_PWD='tefa123' npm start
